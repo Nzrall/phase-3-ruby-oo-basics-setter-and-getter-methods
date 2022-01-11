fcff0e9c5ba1fccb451ec57f613f80342d5d574c
Joshua <wilson.joshua@outlook.com>
2022-01-11 09:10:29 -0800

    commit tree a9b7e476ac4d93d6dd5292fa37caea23ede7476a

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+    attr_accessor
+
+    def name=(new_name)
+        @name = new_name 
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(new_breed)
+        @breed = new_breed
+    end
+
+    def breed
+        @breed
+    end
 
 end
